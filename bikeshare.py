9ef079c84dd8f6617551407d82054050e95794e6
ragadal <ragahdalrowaiti@gmail.com>
2023-02-27 21:36:10 +0300

    added more comments to bikeshare.py file

--- bikeshare.py
+++ bikeshare.py
@@ -7,6 +7,8 @@ CITY_DATA = { 'chicago': 'chicago.csv',
               'new york city': 'new_york_city.csv',
               'washington': 'washington.csv' }
 
+## WELCOME TO MY CODE!
+
 def get_filters():
     """
     Asks user to specify a city, month, and day to analyze.
